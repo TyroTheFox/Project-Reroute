3bf036c71ec8085282de7dda338176f426e0a83b
Twin Tailed Fox <boyinthreeparts@gmail.com>
2017-07-14 00:34:57 +0100

    Added Basic Weapon Functionality
    Added in weapons, being able to switch between them and weapon spread,
    which is effected by Dexterity.

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -3,12 +3,20 @@ using UnityEngine.Networking;
 
 public class PlayerShooting : NetworkBehaviour
 {
-    [SerializeField] float shotCooldown = .3f;
+    //[SerializeField] float shotCooldown = .3f;
     [SerializeField] int killsToWin = 5;
     [SerializeField] Transform firePosition;
     [SerializeField] ShotEffectsManager shotEffects;
     [SerializeField] public int damage = 5;
     public bool disableShoot = false;
+
+    Weapon[] playerWeapons = {
+        new Weapon("Gun1", 1, 5, 1, 0.5f, 1, 10, 50),
+        new Weapon("Gun2", 2, 10, 10, 3, 10, 10, 10),
+        new Weapon("Gun3", 1, 15, 15, 0.9f, 5, 10, 10)
+    };
+    Weapon currentWeapon = new Weapon("Error", 0, 0, 0, 0, 0, 0, 0);
+    int weaponPos = 0;
 
     [SyncVar(hook = "OnScoreChanged")] int score;
 
@@ -20,9 +28,13 @@ public class PlayerShooting : NetworkBehaviour
     {
         player = GetComponent<Player>();
         shotEffects.Initialize();
+        currentWeapon = playerWeapons[0];
+        Debug.Log("Current Weapon: " + currentWeapon.getName());
 
         if (isLocalPlayer)
+        {
             canShoot = true;
+        }
     }
 
     [ServerCallback]
@@ -37,38 +49,63 @@ public class PlayerShooting : NetworkBehaviour
             return;
 
         ellapsedTime += Time.deltaTime;
+        player = GetComponent<Player>();
 
-        if (Input.GetButtonDown("Fire1") && ellapsedTime > shotCooldown && !disableShoot)
+        if (Input.GetButtonDown("Fire1") && ellapsedTime > currentWeapon.getFireRate() && !disableShoot)
         {
             ellapsedTime = 0f;
-            CmdFireShot(firePosition.position, firePosition.forward);
+            CmdFireShot(firePosition.position, firePosition.forward, currentWeapon.getDamage(),
+                currentWeapon.getShots(), currentWeapon.getRange(), currentWeapon.getSpread(), player.Dexterity);
+        }
+
+        if (Input.GetButtonDown("ChangeWeapon"))
+        {
+            weaponPos++;
+            if (weaponPos == playerWeapons.Length)
+            {
+                weaponPos = 0;
+            }
+            currentWeapon = playerWeapons[weaponPos];
+            Debug.Log("Current Weapon: " + currentWeapon.getName());
         }
     }
 
     [Command]
-    void CmdFireShot(Vector3 origin, Vector3 direction)
+    void CmdFireShot(Vector3 origin, Vector3 direction, int damage, int noOfShots, float range,
+        float spread, float dex)
     {
         RaycastHit hit;
 
-        Ray ray = new Ray(origin, direction);
-        Debug.DrawRay(ray.origin, ray.direction * 3f, Color.red, 1f);
-
-        bool result = Physics.Raycast(ray, out hit, 50f);
-
-        if (result)
+        for (int i = 0; i < noOfShots; i++)
         {
-            PlayerHealth enemy = hit.transform.GetComponent<PlayerHealth>();
+            ////  Generate a random XY point inside a circle:
+            //Vector3 newDirection = Random.insideUnitCircle * spread;
+            //newDirection.z = dex * range; // circle is at Z units 
+            //newDirection = transform.TransformDirection(newDirection.normalized);
+            direction = MathCone.RandomInCone(direction, (Mathf.Deg2Rad * (10 - dex)));
+            Ray ray = new Ray(origin, direction);
+            if (isLocalPlayer)
+                Debug.DrawRay(ray.origin, ray.direction * 3f, Color.red, 1f);
 
-            if (enemy != null)
+            bool result = Physics.Raycast(ray, out hit, range);
+
+            if (result)
             {
-                bool wasKillShot = enemy.TakeDamage(damage);
+                PlayerHealth enemy = hit.transform.GetComponent<PlayerHealth>();
 
-                if (wasKillShot && ++score >= killsToWin)
-                    player.Won();
+                if (enemy != null)
+                {
+                    bool wasKillShot = enemy.TakeDamage(damage);
+                    Debug.Log("Damage Landed: " + damage);
+
+                    if (wasKillShot && ++score >= killsToWin)
+                        player.Won();
+                }
             }
+
+            RpcProcessShotEffects(result, hit.point);
         }
 
-        RpcProcessShotEffects(result, hit.point);
     }
 
     [ClientRpc]
@@ -89,6 +126,19 @@ public class PlayerShooting : NetworkBehaviour
 
     public void FireAsBot()
     {
-        CmdFireShot(firePosition.position, firePosition.forward);
+        CmdFireShot(firePosition.position, firePosition.forward, currentWeapon.getDamage(),
+                currentWeapon.getShots(), currentWeapon.getRange(), currentWeapon.getSpread(), player.Dexterity);
+    }
+
+}
+
+public static class MathCone
+{
+    public static Vector3 RandomInCone(this Vector3 dir, float openingAngle)
+    {
+        Vector3 deviation = Random.insideUnitSphere;
+        dir = dir.normalized;
+        deviation -= Vector3.Dot(deviation, dir) * dir;
+        return (dir * Mathf.Cos(openingAngle) + deviation * Mathf.Sin(openingAngle)).normalized;
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class Weapon{
+
+    string name = "Gun";
+    int damage = 0;
+    float spread = 0;
+    int shots = 0;
+    float fireRate = 0;
+    float chargePerShot = 0;
+    float maxCharge = 0;
+    float range = 0;
+
+    // Use this for initialization
+    public Weapon (string name, int damage, float spread, int shots, float fireRate, 
+        float chargePerShot, float maxCharge, float range) {
+        this.name = name;
+        this.damage = damage;
+        this.spread = spread;
+        this.shots = shots;
+        this.fireRate = fireRate;
+        this.chargePerShot = chargePerShot;
+        this.maxCharge = maxCharge;
+        this.range = range;
+	}
+
+    public string getName() { return name; }
+    public int getDamage() { return damage; }
+    public float getSpread() { return spread; }
+    public int getShots() { return shots; }
+    public float getFireRate() { return fireRate; }
+    public float getChargePerShot() { return chargePerShot; }
+    public float getMaxCharge() { return maxCharge; }
+    public float getRange() { return range; }
+
+}
